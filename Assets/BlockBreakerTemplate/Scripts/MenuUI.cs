fc0189f8f34a855cf1cea8cac31e188a2a4986f9
Neil <neil2193@gmail.com>
2023-10-26 11:04:50 +0300

    Compile errors fixed

--- Assets/BlockBreakerTemplate/Scripts/MenuUI.cs
+++ Assets/BlockBreakerTemplate/Scripts/MenuUI.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using UnityEngine.UI;
 
+using UnityEngine.SceneManagement;
+
 public class MenuUI : MonoBehaviour 
 {
 	//Called when the play button is pressed
 	public void PlayButton ()
 	{
-		Application.LoadLevel(1);	//Loads the 'Game' scene to begin the game
+		SceneManager.LoadScene(1); //Loads the 'Game' scene to begin the game
 	}
 
 	//Called when the quit button is pressed
